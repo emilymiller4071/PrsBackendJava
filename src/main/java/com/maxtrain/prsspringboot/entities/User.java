de08948856c23cc32d6dea6b0f3e832b1177d04b
emilymiller4071 <emilyrauch3@gmail.com>
2023-03-11 11:59:33 -0500

    JsonIgnore added to password property

--- src/main/java/com/maxtrain/prsspringboot/entities/User.java
+++ src/main/java/com/maxtrain/prsspringboot/entities/User.java
@@ -1,5 +1,7 @@
 package com.maxtrain.prsspringboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +18,7 @@ public class User {
 	
 	private String userName;
 	
+	@JsonIgnore
 	private String password;
 	
 	private String firstName;
